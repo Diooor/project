bc48ed2559f38184dc3c3f7ba35c7d424c2959bc
dooor <gxt48956@gmail.com>
2021-02-23 20:19:38 +0800

    okokokokok

---
+++ html5_assignment/html5_assignment/js/worker_countdown.js
@@ -0,0 +1,20 @@
+var countdown = 0;
+function timesCount() {
+
+  if(countdown >= 0){
+    self.postMessage(countdown);
+    countdown = countdown - 1;
+    setTimeout(function(){
+      timesCount();
+    }, 1000);
+  }else{
+    self.postMessage(-1);
+  }
+}
+
+self.onmessage = function(e) {
+  // e.data 可接收到傳過來的資料
+  console.log("收到主程式來的資料：" + e.data);
+  countdown = e.data;
+  timesCount();
+}
